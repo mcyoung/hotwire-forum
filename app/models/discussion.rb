c265eabab9b61b840201698a72079909af7cd3c2
Matt Young <mcyoung86@gmail.com>
2022-05-17 10:45:29 -0600

    Realtime functionality

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -2,4 +2,13 @@ class Discussion < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
 
   validates :name, presence: true
+
+  # Following string should match what's on the index.html.erb turbo_stream_from
+  after_create_commit ->  { broadcast_prepend_to "discussions" }
+  after_update_commit ->  { broadcast_replace_to "discussions" }
+  after_destroy_commit -> { broadcast_remove_to "discussions" }
+
+  def to_param
+    "#{id}-#{name.downcase.to_s[0...100]}".parameterize
+  end
 end
