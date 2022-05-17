1af385e31a67dd5504b274b8424184bac3fa779c
Matt Young <mcyoung86@gmail.com>
2022-05-17 11:23:50 -0600

    Add post creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :discussions
+  resources :discussions do
+    resources :posts, only: %i[create], module: :discussions
+  end
 
   root to: "main#index"
 end
