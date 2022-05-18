267d141488e308d3b48904013c570d604481a290
Matt Young <mcyoung86@gmail.com>
2022-05-18 09:37:24 -0600

    Allow notifications to have a count bubble updating

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,21 @@
+# frozen_string_literal: true
+
 class Notification < ApplicationRecord
   include Noticed::Model
   belongs_to :recipient, polymorphic: true
+
+  after_create_commit ->  { broadcast_notification_count }
+  after_destroy_commit -> { broadcast_notification_count }
+
+private
+
+  def broadcast_notification_count
+    broadcast_action_to(
+      "notifications",
+      action: :replace,
+      target: "notifications_count",
+      partial: "shared/notifications_count",
+      locals: { count: recipient.notifications.unread.count }
+    )
+  end
 end
